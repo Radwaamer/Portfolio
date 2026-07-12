1e4a1519600229bd2d2b627cc506da1ca9953fdf
Radwaamer <rororado44@gmail.com>
2026-07-12 20:48:09 +0300

    add new portfolio

---
+++ README.txt
@@ -0,0 +1,17 @@
+Keep your existing photo1659920728.jpeg and images folder.
+
+Add screenshots for the new projects using these filenames:
+arabflc.png
+anngo.png
+ds-law.png
+sudanesejs.png
+30n-group.png
+auroegypt.png
+silkamarket.png
+sawt-news.png
+marjani-mall.png
+dialectic-institution.png
+hariri-construction.png
+dera.png
+
+Put them inside the existing images folder.
--- script.js
+++ script.js
@@ -5,33 +5,30 @@ $(document).ready(function(){
     $('header').toggleClass('toggle');
   });
 
-  $(window).on('scroll load',function(){
+  $(window).on('scroll load', function(){
 
     $('#menu').removeClass('fa-times');
     $('header').removeClass('toggle');
 
     if($(window).scrollTop() > 0){
-      $('.top').show();
+      $('.top').css('display', 'flex');
     }else{
       $('.top').hide();
     }
 
   });
 
-  // smooth scrolling 
-
-  $('a[href*="#"]').on('click',function(e){
+  $('a[href*="#"]').on('click', function(e){
 
     e.preventDefault();
 
-    $('html, body').animate({
+    const target = $(this).attr('href');
 
-      scrollTop : $($(this).attr('href')).offset().top,
-
-    },
-      500, 
-      'linear'
-    );
+    if($(target).length){
+      $('html, body').animate({
+        scrollTop: $(target).offset().top
+      }, 500, 'linear');
+    }
 
   });
 
